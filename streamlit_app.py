98c18ceefe48c74b4167e30490b0f768440e56da
TRAVEL\bgruodys <benjaminas.gruodys@oag.com>
2022-09-29 10:19:04 +0300

    no message

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit
 
 streamlit.title('My parent\'s new healthy bar')
-streamlit.header('header test')
-streamlit.text('adding text')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
